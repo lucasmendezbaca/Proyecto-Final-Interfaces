7f1c99c72dd017af123259557ac64ed824f43116
Lucas Mendez <lucasmendezbaca@gmail.com>
2023-02-13 13:32:24 +0100

    primer commit

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+// require("gulp");
+
+// function holaMundo(cb) {
+//   console.log("Hola mundo");
+//   cb();
+// }
+
+// function adiosMundo(cb) {
+//     console.log("Adios mundo");
+//     cb();
+// }
+
+// exports.holaMundo = holaMundo;
+// exports.adiosMundo = adiosMundo;
+// exports.default = holaMundo;
+
+
+const {series, parallel, src, dest, watch} = require("gulp");
+
+function holaMundo(cb) {
+  console.log("Hola mundo");
+  cb();
+}
+
+function adiosMundo(cb) {
+    console.log("Adios mundo");
+    cb();
+}
+
+// #############
+
+function pipeline() {
+    return src("css/*.css").pipe(dest("up/css"));
+}
+
+function vigila(cb) {
+    watch("css/*.css", holaMundo);
+    cb();
+}
+
+exports.holaMundo = holaMundo;
+exports.adiosMundo = adiosMundo;
+exports.default = holaMundo;
+exports.pipeline = pipeline;
+exports.vigila = vigila;
+exports.serie = series(adiosMundo, holaMundo);
+exports.paralelo = parallel(adiosMundo, holaMundo);
+
+// #####################
+const rename=require("gulp-rename");
+const sassdoc = require("sassdoc");
+const processhtml = require("gulp-processhtml");
+
+function compilar() {
+    return src("scss/main.scss").pipe(sass()).pipe(dest("up/css"));
+}
+
+var doc_options = {
+    dest: "docs"
+}
+
+function generar_docs() {
+    return src("scss/main.scss").pipe(sassdoc(doc_options));
+}
+
+function html() {
+    return src("*html").pepe(processhtml()).pipe(dest("up"));
+}
